0e9633ed23b69210c0140064b6b153984f10153f
Sebastian Gumprich <sebastian.gumprich@telekom.de>
2024-02-06 11:08:17 +0100

    Support multiple reports from trivy
    This change allows the user to configure which reports
    the operator will send to DefectDojo. By default only
    Vulnerability Reports will be sent.

--- src/settings.py
+++ src/settings.py
@@ -56,3 +56,7 @@ DEFECT_DOJO_PRODUCT_NAME: str = os.getenv(
 DEFECT_DOJO_EVAL_PRODUCT_NAME: bool = get_env_var_bool("DEFECT_DOJO_EVAL_PRODUCT_NAME")
 
 DEFECT_DOJO_DO_NOT_REACTIVATE: bool = get_env_var_bool("DEFECT_DOJO_DO_NOT_REACTIVATE")
+
+LOG_LEVEL: str = os.getenv("LOG_LEVEL", "INFO").upper()
+
+REPORTS: list = os.getenv("REPORTS", "vulnerabilityreports").split(",")
